7f6bf04a9bbbea7db39e33ef128298b5db1adf56
Richie-Pursuit <richiecarrasco@pursuit.org>
2023-08-16 12:55:39 -0400

    created card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import BathroomList from './Components/bathroomList/BathroomList';
 
 function App() {
   return (
     <div className="App">
-      <p>HEY WORLD!</p>
+      < BathroomList />
     </div>
   );
 }
---
+++ src/Components/bathroomList/BathroomList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BathroomList.scss';
+import BathroomCard from '../bathroomCard/BathroomCard';
+
+const BathroomList = () => {
+    return (
+        <div className='bathroomList'>
+            <BathroomCard />
+        </div>
+    );
+};
+
+export default BathroomList;
