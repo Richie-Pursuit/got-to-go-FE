3ccef7b5477ee5ba8935c83afd43ad290fb196b2
Richie-Pursuit <richiecarrasco@pursuit.org>
2023-08-16 13:13:36 -0400

    card styled with grid and spaces

--- src/Components/bathroomList/BathroomList.js
+++ src/Components/bathroomList/BathroomList.js
@@ -5,7 +5,14 @@ import BathroomCard from '../bathroomCard/BathroomCard';
 const BathroomList = () => {
     return (
         <div className='bathroomList'>
-            <BathroomCard />
+            <h1 className='bathroomList__title'>Bathrooms</h1>
+            <div className='bathroomList__container' >
+                <BathroomCard />
+                <BathroomCard />
+                <BathroomCard />
+                <BathroomCard />
+                <BathroomCard />
+            </div>
         </div>
     );
 };
